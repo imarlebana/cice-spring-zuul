0022823b62aa5b782b730f143adfd6f706e5c4f1
cice <cice@A8POVMac11.local>
2018-10-31 21:50:13 +0100

    Initial commit

---
+++ src/main/java/com/cice/cicespringzuul/CiceSpringZuulApplication.java
@@ -0,0 +1,17 @@
+package com.cice.cicespringzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class CiceSpringZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiceSpringZuulApplication.class, args);
+    }
+}
